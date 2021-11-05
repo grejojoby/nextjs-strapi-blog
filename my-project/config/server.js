1c68f3a33b3b4a9a9d6a654a47da536cc15b732d
Grejo Joby <grejo00@gmail.com>
2021-11-05 21:15:11 +0530

    push

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed0d048301400d941901f5943a4784e4'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ad16ce27-f286-496b-8e67-dfd9bd0bb2a5'
+};
